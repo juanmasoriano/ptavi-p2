d785304b99340eb4b41f621a4f3687163d62a7c3
juanmasoriano <juanmasg96@gmail.com>
2017-10-02 14:01:14 +0200

    Calcplus

---
+++ calcplus.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+
+import sys
+
+from calc import CalculadoraHija
+
+c = CalculadoraHija()
+
+if __name__ == "__main__":
+    fichero = open("Fichero.txt","w")
+    fichero.write("suma,2,20,10,4,\nresta,15,10,5,\nmultiplica,1,1,5,\ndivide,80,2,0,1")
+    fichero.close()
+
+    fichero = open("Fichero.txt","r")
+
+    for linea in fichero:
+        num = linea.split(",")
+
+        if num[0] == "suma":
+            resultado = int(num[1])
+            for i in num[2:-1]:
+                resultado = resultado + int(i)
+            print(resultado)
+        elif num[0] == "resta":
+            resultado = int(num[1])
+            for i in num[2:-1]:
+                resultado = resultado - int(i)
+            print(resultado)
+        elif num[0] == "multiplica":
+            resultado = int(num[1])
+            for i in num[2:-1]:
+                resultado = resultado * int(i)
+            print(resultado)
+        elif num[0] == "divide":
+            try:
+                resultado = int(num[1])
+                for i in num[2:-1]:
+                    resultado = resultado / int(i)
+                print(resultado)
+            except ZeroDivisionError:
+                sys.exit("Division by zero is not allowed")
+        else:
+            sys.exit('Operación sólo puede ser sumar, restar, multiplicar o dividir.')
