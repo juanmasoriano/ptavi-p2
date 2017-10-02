2bdf063722ae4ecb75d76b73e26b62f4b4c35e58
juanmasoriano <juanmasg96@gmail.com>
2017-10-02 12:24:56 +0200

    modificacion calc.py

--- calc.py
+++ calc.py
@@ -3,16 +3,15 @@
 
 import sys
 
+class Calculadora:
 
-def plus(op1, op2):
-    """ Function to sum the operands. Ops have to be ints """
-    return op1 + op2
+    def plus(self, op1, op2):
+        """ Function to sum the operands. Ops have to be ints """
+        print(op1 + op2)
 
-
-def minus(op1, op2):
-    """ Function to substract the operands """
-    return op1 - op2
-
+    def minus(self, op1, op2):
+        """ Function to substract the operands """
+        print(op1 - op2)
 if __name__ == "__main__":
     try:
         operando1 = int(sys.argv[1])
@@ -26,5 +25,3 @@ if __name__ == "__main__":
         result = minus(operando1, operando2)
     else:
         sys.exit('Operación sólo puede ser sumar o restar.')
-
-    print(result)
